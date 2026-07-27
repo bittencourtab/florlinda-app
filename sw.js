c20767026d25a9d60c6292d824f36a2fe5abf072
bittencourtab <bittencourtab@gmail.com>
2026-07-26 22:51:47 -0300

    Revert layout original + notificacoes de pedidos do bot

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v5';
+const CACHE_NAME = 'florlinda-v6';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo.png'];
 
 self.addEventListener('install', event => {

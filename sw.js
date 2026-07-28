7544511850e89183981ec2a4481842e1fb794861
bittencourtab <bittencourtab@gmail.com>
2026-07-27 22:27:19 -0300

    Fix card layout: 10x10 card + 7x9 Polaroid photos on A4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v13';
+const CACHE_NAME = 'florlinda-v14';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo.png'];
 
 self.addEventListener('install', event => {

f5ddca798d7f9ce4e5c7fc2b1aaad20cba792210
bittencourtab <bittencourtab@gmail.com>
2026-07-27 22:02:04 -0300

    Add editable order detail with all fields + Polaroid photo layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v8';
+const CACHE_NAME = 'florlinda-v11';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo.png'];
 
 self.addEventListener('install', event => {

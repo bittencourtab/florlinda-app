23f8c5aaaf32854c12137db9b5bc6a050d7f5be5
bittencourtab <bittencourtab@gmail.com>
2026-07-27 21:51:20 -0300

    Add printable card and photos layout for orders

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v7';
+const CACHE_NAME = 'florlinda-v8';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo.png'];
 
 self.addEventListener('install', event => {

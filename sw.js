e69ac5aa6491c219a8bc1e5c1025067bdd524782
bittencourtab <bittencourtab@gmail.com>
2026-08-01 16:54:15 -0300

    Atualizar versao do cache v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v18';
+const CACHE_NAME = 'florlinda-v19';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo.png'];
 
 self.addEventListener('install', event => {

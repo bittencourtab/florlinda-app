c400c752889bc9b37e8233baf982ffd220206878
bittencourtab <bittencourtab@gmail.com>
2026-08-01 18:33:28 -0300

    Forcar atualizacao cache v21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v20';
+const CACHE_NAME = 'florlinda-v21';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo.png'];
 
 self.addEventListener('install', event => {

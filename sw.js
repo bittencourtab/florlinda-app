d478a9709783257872a70eb93961b1046b3a6ce2
bittencourtab <bittencourtab@gmail.com>
2026-08-01 16:29:16 -0300

    chore: update cache v18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v17';
+const CACHE_NAME = 'florlinda-v18';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo.png'];
 
 self.addEventListener('install', event => {

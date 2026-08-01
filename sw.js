d70741b5a67416df2496c97cd8e2c36df23bc338
bittencourtab <bittencourtab@gmail.com>
2026-08-01 18:27:54 -0300

    Corrigir ordenacao em showHome e showCatalog

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v19';
+const CACHE_NAME = 'florlinda-v20';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo.png'];
 
 self.addEventListener('install', event => {

2464907159417f00f85179016243f8c0575c88de
bittencourtab <bittencourtab@gmail.com>
2026-08-02 18:08:38 -0300

    v27: Products grid 2 columns on mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v26';
+const CACHE_NAME = 'florlinda-v27';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo-white.png'];
 
 self.addEventListener('install', event => {

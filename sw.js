6a5ceec42508850aabd991f390af4c300c5f273f
bittencourtab <bittencourtab@gmail.com>
2026-08-02 18:13:15 -0300

    v30: Top bar side by side on mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v29';
+const CACHE_NAME = 'florlinda-v30';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo-white.png'];
 
 self.addEventListener('install', event => {

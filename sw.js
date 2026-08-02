56c4160c2e41c279407015f72e31c582087e601b
bittencourtab <bittencourtab@gmail.com>
2026-08-02 18:40:33 -0300

    v36: Fix Instagram feed width

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v35';
+const CACHE_NAME = 'florlinda-v36';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo-white.png'];
 
 self.addEventListener('install', event => {

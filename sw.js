aa2ea502eefcec56b8f9bd875330036c36d06c47
bittencourtab <bittencourtab@gmail.com>
2026-08-02 17:26:12 -0300

    v24: Force cache refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v23';
+const CACHE_NAME = 'florlinda-v24';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo-white.png'];
 
 self.addEventListener('install', event => {

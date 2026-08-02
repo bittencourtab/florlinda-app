b02c199779779c58484a545b442c9bfbb41ec545
bittencourtab <bittencourtab@gmail.com>
2026-08-02 18:31:47 -0300

    v33: Increase Instagram feed height

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'florlinda-v32';
+const CACHE_NAME = 'florlinda-v33';
 const urlsToCache = ['app-pedidos.html', 'manifest.json', 'img/logo-white.png'];
 
 self.addEventListener('install', event => {
